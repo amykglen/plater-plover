09530cf7463c7d390d5a04622c8349bcdad463e7
amykglen <glena@oregonstate.edu>
2023-10-25 16:26:18 -0700

    Also convert edges from tsv to jsonl

--- convert_kg2c_tsv_to_jsonl.py
+++ convert_kg2c_tsv_to_jsonl.py
@@ -1,4 +1,5 @@
 import argparse
+import ast
 import csv
 import json
 import logging
@@ -9,7 +10,7 @@ import pandas as pd
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 ARRAY_DELIMITER = "ǂ"
-ARRAY_COL_NAMES = {"all_names", "all_categories", "equivalent_curies", "publications"}
+ARRAY_COL_NAMES = {"all_names", "all_categories", "equivalent_curies", "publications", "kg2_ids"}
 KGX_COL_NAME_REMAPPINGS = {
     "all_categories": "category",
     "category": "preferred_category"
@@ -22,6 +23,7 @@ logging.basicConfig(level=logging.INFO,
 
 
 def download_kg2c_tsvs():
+    # Only works if you have the aws CLI set up, of course
     logging.info(f"Downloading KG2pre TSV source files..")
     kg2c_tarball_name = "kg2c-tsv.tar.gz"
     logging.info(f"Downloading {kg2c_tarball_name} from the rtx-kg2 S3 bucket")
@@ -36,8 +38,46 @@ def parse_value(value: any, col_name: str):
             return value.split(ARRAY_DELIMITER)
         else:
             return []
+    elif value in {"True", "False", "true", "false"}:
+        return ast.literal_eval(value)
     else:
         return value
+
+
+def convert_tsv_to_jsonl(tsv_filename: str, header_tsv_filename: str):
+    logging.info(f"**** Starting to process file {tsv_filename} (header file is: {header_tsv_filename}) ****")
+
+    jsonl_output_file_name = tsv_filename.replace('.tsv', '.jsonl')
+    logging.info(f"Output filename will be: {jsonl_output_file_name}")
+
+    # First load column names and remove the ':type' suffixes neo4j requires on column names
+    header_df = pd.read_table(f"{SCRIPT_DIR}/{header_tsv_filename}")
+    column_names = [col_name.split(":")[0] if not col_name.startswith(":") else col_name
+                    for col_name in header_df.columns]
+    node_column_indeces = {col_name: index for index, col_name in enumerate(column_names)}
+    logging.info(f"Columns mapped to their indeces are:\n "
+                 f"{json.dumps(node_column_indeces, indent=2)}")
+    columns_to_keep = [col_name for col_name in column_names if not col_name.startswith(":")]
+    logging.info(f"We'll use this subset of ({len(columns_to_keep)}) columns:\n "
+                 f"{json.dumps(columns_to_keep, indent=2)}")
+
+    logging.info(f"Starting to convert rows in {tsv_filename} to json lines..")
+    with open(f"{SCRIPT_DIR}/{tsv_filename}", "r") as input_tsv_file:
+
+        with jsonlines.open(f"{SCRIPT_DIR}/{jsonl_output_file_name}", mode="w") as jsonl_writer:
+
+            tsv_reader = csv.reader(input_tsv_file, delimiter="\t")
+            for line in tsv_reader:
+                row_obj = dict()
+                for col_name in columns_to_keep:
+                    raw_value = line[node_column_indeces[col_name]]
+                    if raw_value not in {"", "{}", "[]", None}:  # Only skip empty values, not false ones
+                        kgx_col_name = KGX_COL_NAME_REMAPPINGS.get(col_name, col_name)
+                        row_obj[kgx_col_name] = parse_value(raw_value, col_name)
+
+                jsonl_writer.write(row_obj)
+
+    logging.info(f"Done converting rows in {tsv_filename} to json lines.")
 
 
 def main():
@@ -49,37 +89,8 @@ def main():
     args = arg_parser.parse_args()
     logging.info(f"Input args are:\n {args}")
 
-    nodes_jsonl_output_file_name = args.nodes_tsv_filename.replace('.tsv', '.jsonl')
-    logging.info(f"Output nodes filename is: {nodes_jsonl_output_file_name}")
-
-    # First load column names and remove the ':type' suffixes neo4j requires on column names
-    nodes_header_df = pd.read_table(f"{SCRIPT_DIR}/{args.nodes_header_tsv_filename}")
-    node_column_names = [column_name.split(":")[0] if not column_name.startswith(":") else column_name
-                         for column_name in nodes_header_df.columns]
-    node_column_indeces = {col_name: index for index, col_name in enumerate(node_column_names)}
-    logging.info(f"Columns mapped to their indeces are:\n "
-                 f"{json.dumps(node_column_indeces, indent=2)}")
-    columns_to_keep = [col_name for col_name in node_column_names if not col_name.startswith(":")]
-    logging.info(f"We'll use this subset of ({len(columns_to_keep)}) columns:\n "
-                 f"{json.dumps(columns_to_keep, indent=2)}")
-
-    logging.info(f"Starting to convert rows in {args.nodes_tsv_filename} to json lines..")
-    with open(f"{SCRIPT_DIR}/{args.nodes_tsv_filename}", "r") as nodes_tsv_file:
-
-        with jsonlines.open(f"{SCRIPT_DIR}/{nodes_jsonl_output_file_name}", mode="w") as jsonl_writer:
-
-            tsv_reader = csv.reader(nodes_tsv_file, delimiter="\t")
-            for line in tsv_reader:
-                node_obj = dict()
-                for col_name in columns_to_keep:
-                    raw_value = line[node_column_indeces[col_name]]
-                    if raw_value != "" and raw_value is not None:  # We don't want to skip false values..
-                        kgx_col_name = KGX_COL_NAME_REMAPPINGS.get(col_name, col_name)
-                        node_obj[kgx_col_name] = parse_value(raw_value, col_name)
-
-                jsonl_writer.write(node_obj)
-
-    logging.info(f"Done converting rows in {args.nodes_tsv_filename} to json lines.")
+    convert_tsv_to_jsonl(args.nodes_tsv_filename, args.nodes_header_tsv_filename)
+    convert_tsv_to_jsonl(args.edges_tsv_filename, args.edges_header_tsv_filename)
 
 
 if __name__ == "__main__":
