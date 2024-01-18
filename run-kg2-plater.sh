2b2b93707d7165723d5104fc8d138ce54d78a826
amykglen <glena@oregonstate.edu>
2024-01-18 09:57:51 -0800

    Use renci neo4j container; add a bit more documentation

--- run-kg2-plater.sh
+++ run-kg2-plater.sh
@@ -1,19 +1,21 @@
 """
 This script loads a given KG2c version into Plater (and also Neo4j, which Plater uses). It downloads
 the KG2c TSV files from arax-databases.rtx.ai.
-For now this script should be run on the kg2cplover2.rtx.ai instance (see markdown in this repo
+For now this script should be run on the kg2cplover2.rtx.ai instance (see README in this repo
 about setting up an instance for this script). Activate the 'plater-plover' pyenv before running
 this script.
-Usage: bash -x run-kg2-plater.sh <kg2_version, e.g., 2.8.4>
+Usage: bash -x run-kg2-plater.sh <kg2_version, e.g., 2.8.4> <neo4j_password>
 """
 
 kg2_version="$1"
+neo4j_password="$2"
+
 orion_kg2_subdir_name=rtx-kg${kg2_version}c
 orion_kg2_subdir_path=~/ORION_parent_dir/Data_services_graphs/${orion_kg2_subdir_name}
 neo4j_container_name=neo4j-kg${kg2_version}c
 
 # Download the proper KG2c TSVs
-cd ~/plater-plover  # This should be the directory containing this script
+cd "$(dirname "$0")"  # This is the directory containing this script ('plater-plover')
 local_kg2c_tarball_name=kg${kg2_version}c-tsv.tar.gz
 scp rtxconfig@arax-databases.rtx.ai:/home/rtxconfig/KG${kg2_version}/extra_files/kg2c-tsv.tar.gz ${local_kg2c_tarball_name}
 tar -xvzf ${local_kg2c_tarball_name}
@@ -32,7 +34,7 @@ export DATA_SERVICES_STORAGE="$PWD/../Data_services_storage/"
 export DATA_SERVICES_GRAPHS="$PWD/../Data_services_graphs/"
 export DATA_SERVICES_LOGS="$PWD/../Data_services_logs/"
 export DATA_SERVICES_GRAPH_SPEC=testing-graph-spec.yml
-export DATA_SERVICES_NEO4J_PASSWORD=test
+export DATA_SERVICES_NEO4J_PASSWORD=${neo4j_password}
 export DATA_SERVICES_OUTPUT_URL=https://localhost/
 export PYTHONPATH="$PYTHONPATH:$PWD"
 printenv
@@ -44,11 +46,12 @@ sudo -E docker-compose run --rm data_services \
 
 # Load the ORION Neo4j dump into a Neo4j database (goes into /home/ubuntu/neo4j/data area..)
 # WARNING: If you don't want /home/ubuntu/neo4j/data to be overwritten, move it before running this part..
+sudo docker pull renciorg/neo4j-4.4.10-apoc-gds:0.0.1
 sudo docker run --interactive --tty --rm \
                 --volume=$HOME/neo4j/data:/data \
                 --volume=$HOME/ORION_parent_dir/Data_services_graphs/${orion_kg2_subdir_name}:/backups \
-                --env NEO4J_AUTH=neo4j/test \
-                neo4j/neo4j-admin:4.4.10 \
+                --env NEO4J_AUTH=neo4j/${neo4j_password} \
+                renciorg/neo4j-4.4.10-apoc-gds:0.0.1 \
                 neo4j-admin load --database=neo4j --from=/backups/graph_.db.dump
 
 # Start up the Neo4j database
@@ -57,12 +60,8 @@ sudo docker run -d \
                 -p 7474:7474 -p 7687:7687 \
                 --name ${neo4j_container_name} \
                 --volume=$HOME/neo4j/data:/data \
-                --env NEO4J_AUTH=neo4j/test \
-                -e NEO4J_apoc_export_file_enabled=true \
-                -e NEO4J_apoc_import_file_enabled=true \
-                -e NEO4J_apoc_import_file_use__neo4j__config=true \
-                -e NEO4J_PLUGINS=\[\"apoc\"\] \
-                neo4j:4.4.10
+                --env NEO4J_AUTH=neo4j/${neo4j_password} \
+                renciorg/neo4j-4.4.10-apoc-gds:0.0.1
 
 # Start up Plater
 cd ~/Plater
