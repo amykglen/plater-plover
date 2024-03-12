b226497c3af1c7ff753512d04e3eaf0d3958116b
amykglen <glena@oregonstate.edu>
2024-03-12 15:37:45 -0700

    Get set up to do batch test run

--- run-tests-plater.sh
+++ run-tests-plater.sh
@@ -1,5 +1,7 @@
 # Meant to be run on the kg2cplover instance (or whatever instance you want to send tests FROM)
 
+is_set_flag=$1
+
 cd ~/plater-plover/test
 rm plater.tsv
 git pull origin main
@@ -7,6 +9,6 @@ git pull origin main
 
 plater_endpoint=http://kg2cplover2.rtx.ai:8080/1.4
 
-pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ITRBPROD --endpoint ${plater_endpoint}
-pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ANYKG2 --endpoint ${plater_endpoint}
-pytest -vs test.py -k test_specified --querypath sample_kg2_queries_LONG --endpoint ${plater_endpoint}
+pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ITRBPROD ${is_set_flag} --endpoint ${plater_endpoint}
+pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ANYKG2 ${is_set_flag} --endpoint ${plater_endpoint}
+pytest -vs test.py -k test_specified --querypath sample_kg2_queries_LONG ${is_set_flag} --endpoint ${plater_endpoint}
--- run-tests-plover.sh
+++ run-tests-plover.sh
@@ -1,5 +1,7 @@
 # Meant to be run on the kg2cplover instance (or whatever instance you want to send tests FROM)
 
+is_set_flag=$1
+
 cd ~/plater-plover/test
 rm plover.tsv
 git pull origin main
@@ -7,6 +9,6 @@ git pull origin main
 
 plover_endpoint=http://kg2cplover2.rtx.ai:8080/api/rtxkg2/v1.4
 
-pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ITRBPROD --endpoint ${plover_endpoint}
-pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ANYKG2 --endpoint ${plover_endpoint}
-pytest -vs test.py -k test_specified --querypath sample_kg2_queries_LONG --endpoint ${plover_endpoint}
+pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ITRBPROD ${is_set_flag} --endpoint ${plover_endpoint}
+pytest -vs test.py -k test_specified --querypath sample_kg2_queries_ANYKG2 ${is_set_flag} --endpoint ${plover_endpoint}
+pytest -vs test.py -k test_specified --querypath sample_kg2_queries_LONG ${is_set_flag} --endpoint ${plover_endpoint}
